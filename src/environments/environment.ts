575fbb84d987ac301ffd378e2b7590be629b6731
YashShah1206 <118794893+YashShah1206@users.noreply.github.com>
2026-03-19 21:18:17 +0530

    Update apiUrl to include '/api' in production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
-// environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://billing-system-api-production.up.railway.app'
+  apiUrl: 'https://billing-system-api-production.up.railway.app/api' // Add /api here
 };
